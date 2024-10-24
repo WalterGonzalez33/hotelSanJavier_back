16c9dadd59de306d795d56d541825c49be8be48f
Leonardo Recalde <leo03recalde@gmail.com>
2024-10-24 14:35:52 -0300

    solicitudes get, put y delete funcionando

--- src/controllers/room.controllers.js
+++ src/controllers/room.controllers.js
@@ -15,3 +15,48 @@ export const createRoom = async (req, res) => {
     res.status(500).json({ mensaje: 'La habitacion no pudo ser creada' })
   }
 }
+
+export const viewRooms = async (req, res) =>{
+  try {
+    const habitaciones = await Room.find();
+    res.status(200).json(habitaciones);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "ocurrio un error, no te pudimos mostrarte las habitaciones" });
+  }
+
+}
+
+export const editRoom = async (req, res) =>{
+  try{
+    const searchRoom = await Room.findById(req.params.id)
+    if (!searchRoom){
+      return res.status(404).json({ mensaje: "La habitación solicitada no existe" });
+       };
+    await Room.findByIdAndUpdate(req.params.id, req.body)
+    res.status(200).json({ mensaje: "La habitación fue modificada con exito" });
+  }catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error, no pudimos hacer las modificaciones solicitadas" });
+  }
+}
+
+export const deleteRoom = async (req, res) =>{
+  try{
+    const searchRoom = await Room.findById(req.params.id)
+    if (!searchRoom){
+      return res.status(404).json({ mensaje: "La habitación solicitada no existe" });
+       };
+    await Room.findByIdAndDelete(req.params.id, req.body)
+    res.status(200).json({ mensaje: "La habitación fue eliminada con exito" });
+  }catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "Ocurrio un error, no pudimos hacer eliminar la habitacion seleccionada" });
+  }
+}
--- src/routes/room.routes.js
+++ src/routes/room.routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express'
-import { createRoom, funcionPrueba } from '../controllers/room.controllers.js'
-
+import { createRoom, funcionPrueba,editRoom, viewRooms, deleteRoom } from '../controllers/room.controllers.js'
 const roomRouter = Router()
 
 roomRouter.route('/prueba').get(funcionPrueba)
-roomRouter.route('/rooms').post(createRoom)
+roomRouter.route('/rooms').post(createRoom).get(viewRooms)
+roomRouter.route('/rooms/:id').put(editRoom).delete(deleteRoom)
 
 export default roomRouter
