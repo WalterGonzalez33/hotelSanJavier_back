f684bd0e6f3a4252156019063ebdfe899121a87b
Mauro Escritorio <mauroconiglioo48@gmail.com>
2024-10-28 12:31:42 -0300

    cree el archivo Validation Room e hice las validaciones del modelo de la habitacion, falta terminar una validacion unicamente

---
+++ src/validations/validationRooms.js
@@ -0,0 +1,38 @@
+import { handleValidationResult } from "../helpers/validationResult.js";
+import { check } from "express-validator";
+
+export const roomsValidations = [
+    // check('room_name')
+    // .notEmpty()
+    // .withMessage('skljdfgns'),
+    check('price')
+    .notEmpty()
+    .withMessage('El precio es obligatorio')
+    .isNumeric()
+    .withMessage('El precio tiene que ser un numero')
+    .isLength({min:5000 , max:1000000 })
+    .withMessage('El precio minimo es de 5.000 y maximo de 1.000.000'),
+    check('number_rooms')
+    .notEmpty()
+    .withMessage('El numero de habitacion es obligatorio')
+    .matches(/^(1[0-9]|20|[1-9])$/)
+    .withMessage('el numero no tiene el formato valido'),
+    check('image')
+    .notEmpty()
+    .withMessage('La imagen es un dato obligatorio')
+    .matches(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/)
+    .withMessage('El formato de la imagen no es el correcto'),
+    check('brief_description')
+    .notEmpty()
+    .withMessage('La breve descripcion es obligatoria')
+    .isLength({min:2 , max:100})
+    .withMessage('Debe tener un minimo de 2 caracteres y un maximo de 100'),
+    check('broad_description')
+    .notEmpty()
+    .withMessage('La descripcion amplia es obligatoria')
+    .isLength({min:50 , max:1000})
+    .withMessage('Debe tener un minimo de 50 caracteres y un maximo de 1000'),
+    (req,res,next) => handleValidationResult(req,res,next)
+    
+
+]
