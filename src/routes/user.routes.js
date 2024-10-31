1821e216438f6a3fa8c7310b2e6001ee35daa89d
Leonardo Recalde <leo03recalde@gmail.com>
2024-10-30 21:20:29 -0300

    se borra usuario

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { createUser, userList } from "../controllers/userController.js";
+import { createUser, userDelete, userList } from "../controllers/userController.js";
 const router = Router()
 router.route('/users').post(createUser).get(userList);
+router.route('/users/:id').delete(userDelete);
 
 export default router;
