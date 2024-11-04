134f5311e1d15523e9fc07a1ed3a6d396b06a5a9
Walter Osvaldo Gonzalez <wg904093@gmail.com>
2024-11-04 11:23:20 -0300

    ✨ [FEAT] se modifico la propiedad username del modelo del usuario

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,82 +1,75 @@
-import User from "../database/model/user.js";
-import bcrypt from "bcrypt";
-
+import User from '../database/model/user.js'
+import bcrypt from 'bcrypt'
 
 export const createUser = async (req, res) => {
   try {
     // agregar validaciones
     // Verifico si el mail ya fue registrado
-    const { email, username, password , status, roll } = req.body;
-    const usuarioExistente = await User.findOne({ email });
+    const { email, username, password, status, roll } = req.body
+    const usuarioExistente = await User.findOne({ email })
     if (usuarioExistente) {
-        return res
-          .status(400)
-          .json({ mensaje: "Este correo ya se encuentra registrado" });
-      }
+      return res
+        .status(400)
+        .json({ mensaje: 'Este correo ya se encuentra registrado' })
+    }
 
     // crear el usuario
-    const newUser = new User(req.body);
+    const newUser = new User(req.body)
     // Hashear el password
-    const saltos = bcrypt.genSaltSync(10);
-    newUser.password = bcrypt.hashSync(password, saltos);
-    newUser.save();
+    const saltos = bcrypt.genSaltSync(10)
+    newUser.password = bcrypt.hashSync(password, saltos)
+    newUser.save()
     // Enviar una respuesta afirmativa
-    res.status(201).json({ mensaje: "El usuario se creo correctamente" });
+    res.status(201).json({ mensaje: 'El usuario se creo correctamente' })
   } catch (error) {
-    console.error(error);
+    console.error(error)
     res
       .status(400)
-      .json({ mensaje: "Ocurrio un error al intentar crear un usuario" });
+      .json({ mensaje: 'Ocurrio un error al intentar crear un usuario' })
   }
-};
+}
 
 export const login = async (req, res) => {
   try {
-    //agregar validaciones
-    //verificar si el mail ya fue registrado
-    const { email, password } = req.body;
-    const usuarioExistente = await User.findOne({ email });
+    // agregar validaciones
+    // verificar si el mail ya fue registrado
+    const { email } = req.body
+    const usuarioExistente = await User.findOne({ email })
     if (!usuarioExistente) {
       return res
         .status(400)
-        .json({ mensaje: "Correo o password incorrecto - email" });
+        .json({ mensaje: 'Correo o password incorrecto - email' })
     }
-    //quiero saber si el password en incorrecto
-    if (!passwordValido) {
-      return res
-        .status(400)
-        .json({ mensaje: "Correo o password incorrecto - password" });
-    }
+
     // generar un token
     // respodemos afirmativamente
     res.status(200).json({
-      mensaje: "Los datos del usuario son validos",
-      email
-    });
+      mensaje: 'Los datos del usuario son validos',
+      id: usuarioExistente._id
+    })
   } catch (error) {
-    console.error(error);
+    console.error(error)
     res
       .status(500)
-      .json({ mensaje: "Ocurrio un error al intentar loguear a un usuario" });
+      .json({ mensaje: 'Ocurrio un error al intentar loguear a un usuario' })
   }
-};
+}
 
-export const userList = async (req,res) =>{
-    try{
-      const users = await User.find();
-      res.status (200).json(users);
+export const userList = async (req, res) => {
+  try {
+    const users = await User.find()
+    res.status(200).json(users)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ mensaje: 'ocurrio un error no se pudo crear el usuario' })
+  }
+}
 
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({mensaje: "ocurrio un error no se pudo crear el usuario"})
-    }
-};
-
-export const userDelete = async(req, res) =>{
+export const userDelete = async (req, res) => {
   try {
     const searchUser = await User.findById(req.params.id)
     if (!searchUser) {
-      return res.status(404).json({ mensaje: 'El usuario selicitado no existe' })
+      return res.status(404).json({ mensaje: 'El usuario solicitado no existe' })
     };
     await User.findByIdAndDelete(req.params.id, req.body)
     res.status(200).json({ mensaje: 'El usuario fue eliminado con éxito' })
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
-import { Router } from "express";
-import { createUser, userDelete, userList } from "../controllers/userController.js";
+import { Router } from 'express'
+import { createUser, userDelete, userList } from '../controllers/userController.js'
 const router = Router()
-router.route('/users').post(createUser).get(userList);
-router.route('/users/:id').delete(userDelete);
+router.route('/users').post(createUser).get(userList)
+router.route('/users/:id').delete(userDelete)
 
-export default router;
+export default router
