57aef8a8d5aaae70be26f5a2cb2aa38b96733dd9
Walter Osvaldo Gonzalez <wg904093@gmail.com>
2024-11-05 14:04:40 -0300

    🔥 [FIX] comflict

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -72,14 +72,12 @@ export const login = async (req, res) => {
     }
     // generar un token
     const token = await generarJWT(
-      usuarioExistente._id,
-      usuarioExistente.email
+      usuarioExistente._id
     )
     // respodemos afirmativamente
     res.status(200).json({
       mensaje: 'Los datos del usuario son validos',
       token
-      // id: usuarioExistente._id
     })
   } catch (error) {
     console.error(error)
