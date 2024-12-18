ad942829354cdb4e448247fb088aa9fcca6b4ab2
Walter Osvaldo Gonzalez <wg904093@gmail.com>
2024-12-18 14:09:02 -0300

    🐛 [FIX] se configuro CORS para manejar preflight

--- index.js
+++ index.js
@@ -20,7 +20,24 @@ app.set('port', process.env.PORT || 4000)
 const port = app.get('port')
 
 // config de middleware
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
+app.options('*', cors())
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end()
+  }
+
+  next()
+})
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
